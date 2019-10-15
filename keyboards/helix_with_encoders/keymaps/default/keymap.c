7690a2087fb3ac7636c8c2b41e50480b72ac1937
avatak <shultzwm@gmail.com>
2019-10-14 20:21:14 -0400

    Updated helix with encoders oled logic

--- keyboards/helix_with_encoders/keymaps/default/keymap.c
+++ keyboards/helix_with_encoders/keymaps/default/keymap.c
@@ -451,6 +451,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 #ifdef ENCODER_ENABLE
+
     void encoder_update_user(uint8_t index, bool clockwise) {
 
     // LEFT KNOB
@@ -534,69 +535,75 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (is_keyboard_master())
-    return OLED_ROTATION_270;
-  return rotation;
-}
+// OLED Driver Logic
+#ifdef OLED_DRIVER_ENABLE
+    oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+        if (is_keyboard_master())
+            return OLED_ROTATION_270;
+        else if (!is_keyboard_master())
+            return OLED_ROTATION_180;
+        else 
+          return rotation;
+    }
 
 static void render_logo(void) {
-  static const char PROGMEM qmk_logo[] = {
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-
-  oled_write_P(qmk_logo, false);
+    static const char PROGMEM sol_logo[] = {
+        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
+    };
+    oled_write_P(sol_logo, false);
 }
 
 static void render_status(void) {
   // Render to mode icon
-  
   static const char PROGMEM sol_icon[] = {
     0x9b,0x9c,0x9d,0x9e,0x9f,
     0xbb,0xbc,0xbd,0xbe,0xbf,
     0xdb,0xdc,0xdd,0xde,0xdf,0
   };
   oled_write_P(sol_icon, false);
-  
 
-  // Define layers here
-  oled_write_P(PSTR("Layer\n"), false);
-  switch (get_highest_layer(layer_state)) {
+
+
+
+// Define layers here
+oled_write_P(PSTR("Layer\n"), false);
+switch (get_highest_layer(layer_state)) {
     case _QWERTY:
-      oled_write_P(PSTR("QWRTY"), false);
-      break;
+        oled_write_P(PSTR("QWRTY"), false);
+        break;
     case _COLEMAK:
-      oled_write_P(PSTR("COLMK"), false);
-      break;
+        oled_write_P(PSTR("COLMK"), false);
+        break;
     case _COLEMAC:
-      oled_write_P(PSTR("CLMAC"), false);
-      break;
+        oled_write_P(PSTR("CLMAC"), false);
+        break;
     case _TEX:
-      oled_write_P(PSTR("LaTeX"), false);
-      break;
+        oled_write_P(PSTR("LaTeX"), false);
+        break;
     case _SYM:
-      oled_write_P(PSTR("SYM  "), false);
-      break;
+        oled_write_P(PSTR("SYM  "), false);
+        break;
     case _NAV:
-      oled_write_P(PSTR("NAV  "), false);
-      break;
+        oled_write_P(PSTR("NAV  "), false);
+        break;
     case _MNAV:
-      oled_write_P(PSTR("MNAV "), false);
-      break;
+        oled_write_P(PSTR("MNAV "), false);
+        break;
     case _NUM:
-      oled_write_P(PSTR("NUM  "), false);
-      break;
+        oled_write_P(PSTR("NUM  "), false);
+        break;
     case _MWIN:
-      oled_write_P(PSTR("MWIN "), false);
-      break;
+        oled_write_P(PSTR("MWIN "), false);
+        break;
     case _ADJUST:
-      oled_write_P(PSTR("ADJST"), false);
-      break;
+        oled_write_P(PSTR("ADJST"), false);
+        break;
     default:
-      oled_write_P(PSTR("UNDEF"), false);
-  }
-
+        oled_write_P(PSTR("UNDEF"), false);
+}
+    
   // Host Keyboard LED Status
     uint8_t led_state = host_keyboard_leds();
     oled_write_P(PSTR("\n-----\n"), false);
@@ -604,10 +611,15 @@ static void render_status(void) {
     oled_write_P(IS_LED_ON(led_state, USB_LED_CAPS_LOCK) ? PSTR("CAPLK") : PSTR("     "), false);
     oled_write_P(IS_LED_ON(led_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLK") : PSTR("     "), false);
 
+#ifdef RGB_OLED_MENU
+    static char buffer[31] = { 0 };
+    snprintf(buffer, sizeof(buffer), "h%3d s%3d v%3d s%3d m%3d e%3d ", rgb_matrix_config.hsv.h, rgb_matrix_config.hsv.s, rgb_matrix_config.hsv.v, rgb_matrix_config.speed, rgb_matrix_config.mode, rgb_matrix_get_flags());
+    buffer[4 + rgb_encoder_state * 5] = '<';
+
+    oled_write_P(PSTR("-----"), false);
+    oled_write(buffer, false);
+#endif
 }
-
-
-
 
 void oled_task_user(void) {
   if (is_keyboard_master()) {
@@ -618,14 +630,7 @@ void oled_task_user(void) {
   }
 }
 
-
-
-
-
-        
-
-
-
+#endif
 
 
 
