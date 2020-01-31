1e36237def51fca043ced572be895610db6eb6e5
Michael S <shultzwm@gmail.com>
2020-01-31 00:10:49 -0500

    Updated helix

--- keyboards/helix_with_encoders/keymaps/default/keymap.c
+++ keyboards/helix_with_encoders/keymaps/default/keymap.c
@@ -135,11 +135,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
     [_COLEMAK] = LAYOUT(/* Base */
-        KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-        KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                   KC_J   , KC_L   , KC_U   , KC_Y   , GUISCLN, KC_BSLS,
-        NAVESC , KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                   KC_K   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , _______, _______, KC_M   , KC_H   , ALT_COM, KC_DOT , CTLSLSH, KC_RSFT,
-        ADJUST , KC_LCTL, _______, KC_LGUI, KC_LALT, NAVSPC , NUM_TAP, SYMENT , TEXSPC , KC_HYPR, KC_SPC , ALTSHFT, ALTCTL , ADJUST
+        KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
+        KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                     KC_J   , KC_L   , KC_U   , KC_Y   , GUISCLN, KC_BSLS,
+        NAVESC , KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                     KC_K   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_TAB , S(KC_TAB), KC_M   , KC_H   , ALT_COM, KC_DOT , CTLSLSH, KC_RSFT,
+        ADJUST , KC_LCTL, _______, KC_LGUI, KC_LALT, NAVSPC , NUM_TAP,   SYMENT , TEXSPC , KC_HYPR, KC_SPC , ALTSHFT, ALTCTL , ADJUST
     ),
 
 
