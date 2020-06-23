2115bea895fb70b4dcf127feee0633c9de6dc664
Michael S <shultzwm@gmail.com>
2020-06-23 09:29:01 -0400

    helix with encoders clicky

--- keyboards/helix_with_encoders/keymaps/avatak/keymap.c
+++ keyboards/helix_with_encoders/keymaps/avatak/keymap.c
@@ -1,34 +1,20 @@
+ /* Copyright 2019 Michael Shultz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
-#include "bootloader.h"
 #include "avatak.h"
-
-#ifdef PROTOCOL_LUFA
-    #include "lufa.h"
-    #include "split_util.h"
-#endif
-
-#ifdef AUDIO_ENABLE
-    #include "audio.h"
-#endif
-
-#ifdef SSD1306OLED
-    #include "ssd1306.h"
-#endif
-
-
-
-#ifdef RGBLIGHT_ENABLE
-  //Following line allows macro to read current RGB settings
-  extern rgblight_config_t rgblight_config;
-#endif
-
-extern uint8_t is_master;
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -173,194 +159,123 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-
-
-
-
-
-#ifdef AUDIO_ENABLE
-    float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-    float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
-    float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
-    float tone_plover[][2]     = SONG(PLOVER_SOUND);
-    float tone_plover_gb[][2]  = SONG(PLOVER_GOODBYE_SOUND);
-    float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
-#endif
-
-// define variables for reactive RGB
-bool TOG_STATUS = false;
-int RGB_current_mode;
-
 void matrix_init_keymap(void) {
-    #ifdef AUDIO_ENABLE
-        startup_user();
-    #endif
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
-    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-    #ifdef SSD1306OLED
-        iota_gfx_init(!has_usb());   // turns on the display
-    #endif
 }
 
+void matrix_scan_keymap(void) {}
 
-#ifdef AUDIO_ENABLE
+void led_set_user(uint8_t usb_led) {}
 
-    void startup_user()
-    {
-        _delay_ms(20); // gets rid of tick
-    }
-
-    void shutdown_user()
-    {
-        _delay_ms(150);
-        stop_all_notes();
-    }
-
-    void music_on_user(void)
-    {
-        music_scale_user();
-    }
-
-    void music_scale_user(void)
-    {
-        PLAY_SONG(music_scale);
-    }
-
-#endif
+// Will be used for maintaining same shortcuts in defferent operating systems
 
 
-//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
-#ifdef SSD1306OLED
 
-void matrix_scan_keymap(void) {
-     iota_gfx_task();  // this is what updates the display continuously
+uint16_t key_timer;
+bool key_triggered = false;
+
+
+
+// OLED Driver Logic
+#ifdef OLED_DRIVER_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_master())
+        return OLED_ROTATION_270;
+    else if (!is_keyboard_master())
+        return OLED_ROTATION_180;
+    else
+        return rotation;
 }
 
-void matrix_update(struct CharacterMatrix *dest,
-                          const struct CharacterMatrix *source) {
-  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-    memcpy(dest->display, source->display, sizeof(dest->display));
-    dest->dirty = true;
-  }
-}
+// Render to mode icon
+  static const char PROGMEM sol_icon[] = {
+    0x9b,0x9c,0x9d,0x9e,0x9f,
+    0xbb,0xbc,0xbd,0xbe,0xbf,
+    0xdb,0xdc,0xdd,0xde,0xdf,0
+  };
 
-//assign the right code to your layers for OLED display
-#define L_COLEMAK 0
-#define L_NAV (1<<_NAV)
-#define L_TEX (1<<_TEX)
-#define L_SYM (1<<_SYM)
-#define L_NUM (1<<_NUM)
-#define L_ADJUST (1<<_ADJUST)
 
-static void render_logo(struct CharacterMatrix *matrix) {
-
-  static const char helix_logo[] PROGMEM ={
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,
-    0};
-  matrix_write_P(matrix, helix_logo);
-  //matrix_write_P(&matrix, PSTR(" Split keyboard kit"));
-}
-
-static void render_rgbled_status(bool full, struct CharacterMatrix *matrix) {
-#ifdef RGBLIGHT_ENABLE
-  char buf[30];
-  if (RGBLIGHT_MODES > 1 && rgblight_config.enable) {
-      if (full) {
-          snprintf(buf, sizeof(buf), " LED %2d: %d,%d,%d ",
-                   rgblight_config.mode,
-                   rgblight_config.hue/RGBLIGHT_HUE_STEP,
-                   rgblight_config.sat/RGBLIGHT_SAT_STEP,
-                   rgblight_config.val/RGBLIGHT_VAL_STEP);
-      } else {
-          snprintf(buf, sizeof(buf), "[%2d] ",rgblight_config.mode);
-      }
-      matrix_write(matrix, buf);
-  }
-#endif
-}
-
-static void render_layer_status(struct CharacterMatrix *matrix) {
-  // Define layers here, Have not worked out how to have text displayed for each layer. Copy down the number you see and add a case for it below
-  char buf[10];
-  matrix_write_P(matrix, PSTR("Layer: "));
-    switch (layer_state) {
-        case L_COLEMAK:
-           matrix_write_P(matrix, PSTR("Colemak"));
-           break;
-        case L_NAV:
-           matrix_write_P(matrix, PSTR("Nav    "));
-           break;
-        case L_TEX:
-           matrix_write_P(matrix, PSTR("LaTeX  "));
-           break;
-        case L_SYM:
-           matrix_write_P(matrix, PSTR("Symbol "));
-           break;
-        case L_NUM:
-           matrix_write_P(matrix, PSTR("Numpad "));
-           break;
-        case L_ADJUST:
-           matrix_write_P(matrix, PSTR("Adjust "));
-           break;
+void render_layer_state(void) {
+    oled_write_P(PSTR("\nLayer"), false);
+    oled_write_P(PSTR("-----"), false);
+    uint8_t layer = layer_state ? biton(layer_state) : biton32(default_layer_state);
+    switch (layer) {
+        case _COLEMAK:
+            oled_write_P(PSTR("COLMK"), false);
+            break;
+        case _COLEMAC:
+            oled_write_P(PSTR("CLMAC"), false);
+            break;
+        case _NAV:
+            oled_write_P(PSTR("  NAV"), false);
+            break;
+        case _TEX:
+            oled_write_P(PSTR("LaTeX"), false);
+            break;
+        case _MNAV:
+            oled_write_P(PSTR(" MNAV"), false);
+            break;
+        case _SYM:
+            oled_write_P(PSTR("SYMBL"), false);
+            break;
+        case _NUM:
+            oled_write_P(PSTR("NUMPD"), false);
+            break;
+        case _MWIN:
+            oled_write_P(PSTR(" MWIN"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("  ADJ"), false);
+            break;
         default:
-           matrix_write_P(matrix, PSTR("Undef-"));
-           snprintf(buf,sizeof(buf), "%ld", layer_state);
-           matrix_write(matrix, buf);
+            oled_write_P(PSTR("UNDEF"), false);
+        }
+    oled_write_P(PSTR("\n"), false);
+}
+
+void render_keylock_status(uint8_t led_usb_state) {
+    uint8_t led_state = host_keyboard_leds();
+    oled_write_P(PSTR("Locks"), false);
+    oled_write_P(PSTR("-----"), false);
+    oled_write_P(IS_LED_ON(led_state, USB_LED_NUM_LOCK) ? PSTR("N") : PSTR(" "), false);
+    oled_write_P(PSTR(" "), false);
+    oled_write_P(IS_LED_ON(led_state, USB_LED_CAPS_LOCK) ? PSTR("C") : PSTR(" "), false);
+    oled_write_P(PSTR(" "), false);
+    oled_write_P(IS_LED_ON(led_state, USB_LED_SCROLL_LOCK) ? PSTR("S") : PSTR(" "), false);
+}
+
+void render_mod_status(uint8_t modifiers) {
+    oled_write_P(PSTR("\nMods "), false);
+    oled_write_P(PSTR("-----"), false);
+    oled_write_P((modifiers & MOD_MASK_SHIFT) ? PSTR("S") : PSTR(" "), false);
+    oled_write_P((modifiers & MOD_MASK_CTRL) ? PSTR("C") : PSTR(" "), false);
+    oled_write_P((modifiers & MOD_MASK_ALT) ? PSTR("A") : PSTR(" "), false);
+    oled_write_P((modifiers & MOD_MASK_GUI) ? PSTR("G") : PSTR(" "), false);
+}
+
+void render_status_main(void) {
+    /* Show Keyboard Layout  */
+    render_layer_state();
+    render_keylock_status(host_keyboard_leds());
+    render_mod_status(get_mods());
+}
+
+static void render_logo(void) {
+    static const char PROGMEM sol_logo[] = {
+        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
+    };
+    oled_write_P(sol_logo, false);
+}
+
+void oled_task_user(void) {
+    if (is_keyboard_master()) {
+        oled_write_P(sol_icon, false);
+        render_status_main();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+    } else {
+        render_logo();
+        //oled_scroll_left();
     }
-}
-
-void render_status(struct CharacterMatrix *matrix) {
-
-  // Render to mode icon
-  static const char os_logo[][2][3] PROGMEM  ={{{0x95,0x96,0},{0xb5,0xb6,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
-  if(keymap_config.swap_lalt_lgui==false){
-    matrix_write_P(matrix, os_logo[0][0]);
-    matrix_write_P(matrix, PSTR("\n"));
-    matrix_write_P(matrix, os_logo[0][1]);
-  }else{
-    matrix_write_P(matrix, os_logo[1][0]);
-    matrix_write_P(matrix, PSTR("\n"));
-    matrix_write_P(matrix, os_logo[1][1]);
-  }
-
-  matrix_write_P(matrix, PSTR(" "));
-  render_layer_status(matrix);
-  matrix_write_P(matrix, PSTR("\n"));
-
-  // Host Keyboard LED Status
-  matrix_write_P(matrix, (host_keyboard_leds() & (1<<USB_LED_NUM_LOCK)) ?
-                 PSTR("NUMLOCK") : PSTR("       "));
-  matrix_write_P(matrix, (host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK)) ?
-                 PSTR("CAPS") : PSTR("    "));
-  matrix_write_P(matrix, (host_keyboard_leds() & (1<<USB_LED_SCROLL_LOCK)) ?
-                 PSTR("SCLK") : PSTR("    "));
-  matrix_write_P(matrix, PSTR("\n"));
-  render_rgbled_status(true, matrix);
-}
-
-
-void iota_gfx_task_user(void) {
-  struct CharacterMatrix matrix;
-
-#if DEBUG_TO_SCREEN
-  if (debug_enable) {
-    return;
-  }
-#endif
-
-  matrix_clear(&matrix);
-  if(is_master){
-    render_status(&matrix);
-  }else{
-    render_logo(&matrix);
-    render_rgbled_status(false, &matrix);
-    render_layer_status(&matrix);
-  }
-  matrix_update(&display, &matrix);
 }
 
 #endif
