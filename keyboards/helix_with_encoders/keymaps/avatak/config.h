449bdf71c4de6ab25e9b7fe5a2260f8cf7fc708f
Michael S <shultzwm@gmail.com>
2020-07-18 19:52:08 -0400

    helix with encoders

--- keyboards/helix_with_encoders/keymaps/avatak/config.h
+++ keyboards/helix_with_encoders/keymaps/avatak/config.h
@@ -21,9 +21,34 @@
 #undef OLED_FONT_H
 #define OLED_FONT_H "keyboards/helix_with_encoders/glcdfont.c"
 
-//#define B7_AUDIO
+#define B7_AUDIO
 //#define AUDIO_CLICKY
 //#define NO_MUSIC_MODE
+
+#define TAPPING_TERM 150
+
+#define TAPPING_TOGGLE 2
+
+
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+//#undef RGBLIGHT_ANIMATIONS
+// /*== or choose animations ==*/
+//   #define RGBLIGHT_EFFECT_BREATHING
+//   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//   #define RGBLIGHT_EFFECT_SNAKE
+//   #define RGBLIGHT_EFFECT_KNIGHT
+//   #define RGBLIGHT_EFFECT_CHRISTMAS
+//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//   #define RGBLIGHT_EFFECT_RGB_TEST
+//   #define RGBLIGHT_EFFECT_ALTERNATING
+
+//#define NO_DEBUG
+//#define NO_PRINT
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
 
 #undef ENCODERS_PAD_A
 #undef ENCODERS_PAD_B
@@ -32,3 +57,8 @@
 #define ENCODERS_PAD_B { B5 }
 
 #define SPLIT_USB_DETECT
+
+#define QMK_ESC_OUTPUT F4
+#define QMK_ESC_INPUT D7
+#define QMK_LED B0
+#define QMK_SPEAKER B7
