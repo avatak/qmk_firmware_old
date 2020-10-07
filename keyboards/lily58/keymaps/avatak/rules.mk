d257bb99df465b8b2d4207b5ef69d9cb631799c3
Michael S <shultzwm@gmail.com>
2020-10-07 03:11:29 -0400

    lily58 progress.

--- keyboards/lily58/keymaps/avatak/rules.mk
+++ keyboards/lily58/keymaps/avatak/rules.mk
@@ -21,10 +21,10 @@ OLED_DRIVER_ENABLE= yes     # OLED display
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+#SRC +=  ./lib/rgb_state_reader.c \
+#        ./lib/layer_state_reader.c \
+#        ./lib/logo_reader.c \
+#        ./lib/keylogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
