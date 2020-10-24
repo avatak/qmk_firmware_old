e07ddba154697bb12a33128bd14d17e1329776c7
Michael S <shultzwm@gmail.com>
2020-10-24 19:07:48 +0000

    Updated helix

---
+++ keyboards/helix_with_encoders/keymaps/avatak2/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define TAPPING_TERM 150
+
+#define TAPPING_TOGGLE 2
+
+
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#undef  ENCODERS_PAD_A
+#undef  ENCODERS_PAD_B
+
+#define ENCODERS_PAD_A { F0 }
+#define ENCODERS_PAD_B { D5 }
