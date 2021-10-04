bd5bb4bd97572e6aaaf3fa731ea02fafcf2036fd
Michael S <shultzwm@gmail.com>
2021-10-03 23:08:09 -0400

    Small change?

--- users/avatak/encoder_custom.c
+++ users/avatak/encoder_custom.c
@@ -1,7 +1,7 @@
 #include "encoder_custom.h"
 
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     /* Left encoder */
     if (index == 0) {
         switch(get_highest_layer(layer_state)) {
@@ -153,4 +153,5 @@ void encoder_update_user(uint8_t index, bool clockwise) {
                 break;
         }
     }
+    return false;
 }
