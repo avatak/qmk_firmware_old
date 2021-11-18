a51b36c047a7173c2d8cfe441b17e26e2739bf1d
Michael S <shultzwm@gmail.com>
2021-11-18 06:48:17 -0500

    Update 11/18

--- keyboards/keebio/nyquist/keymaps/avatak/config.h
+++ keyboards/keebio/nyquist/keymaps/avatak/config.h
@@ -1,9 +1,5 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-Copyright 2018 Danny Nguyen <danny@keeb.io>
+Copyright 2017 Danny Nguyen <danny@hexwire.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,19 +17,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define ENCODERS_PAD_A { F5 }
-//#define ENCODERS_PAD_B { F4 }
-
-//#define ENCODERS_PAD_A_RIGHT { F4 }
-//#define ENCODERS_PAD_B_RIGHT { F5 }
-
-#define RGBLED_NUM 12    // Number of LEDs
-#define RGBLED_SPLIT { 6, 6 }
-
-#define C6_AUDIO
-
 // #define USE_I2C
 
 /* Select hand configuration */
 // #define MASTER_RIGHT
 // #define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
